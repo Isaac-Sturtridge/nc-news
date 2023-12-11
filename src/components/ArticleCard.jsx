e02c187081df21af8397253c1714cdf189be8536
Isaac-Sturtridge <eyes330@yahoo.co.uk>
2023-12-11 15:28:46 +0000

    rejig link

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,9 +1,13 @@
-const ArticleCard = ({article}) => {
+import { Link } from "react-router-dom";
+
+const ArticleCard = ({ article }) => {
   return (
     // very basic layout, to be updated later
     <article className="article_card">
-      <h2>{article.title}</h2>
-      <img src={article.article_img_url} alt={article.title} />
+      <Link to={`/articles/${article.article_id}`}>
+        <h2>{article.title}</h2>
+        <img src={article.article_img_url} alt={article.title} />
+      </Link>
       <h3>{article.author}</h3>
       <h4>{article.topic}</h4>
       <p>
--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import ArticleCard from "./ArticleCard";
 import { getArticles } from "../utils/axios";
-import { Link } from "react-router-dom";
 
 const ArticleList = () => {
   const [articles, setArticles] = useState([]);
@@ -21,9 +20,7 @@ const ArticleList = () => {
       <h1>Article List</h1>
       {articles.map((article) => {
 
-        return <Link to={`/articles/${article.article_id}`}>
-        <ArticleCard key={article.article_id} article={article} />;
-        </Link>
+        return <ArticleCard key={article.article_id} article={article} />;
       })}
     </section>
   );
