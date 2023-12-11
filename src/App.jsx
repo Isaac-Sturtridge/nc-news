3c12eb1a8b3c780f6e542560f1e233229af0a700
Isaac-Sturtridge <eyes330@yahoo.co.uk>
2023-12-11 11:12:41 +0000

    further basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-function App() {
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
+function App() {
   return (
     <>
-    <h1>App</h1>
+      <Header />
+      <Navbar />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
