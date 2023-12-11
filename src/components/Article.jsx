1700e1ce8bb46ad488ee8ccca30ea0f3ca3aa6b6
Isaac-Sturtridge <eyes330@yahoo.co.uk>
2023-12-11 13:56:25 +0000

    displays article body

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -4,20 +4,31 @@ import axios from "axios";
 
 const Article = () => {
   const [article, setArticle] = useState({});
-  const {article_id} = useParams();
+  const { article_id } = useParams();
 
   useEffect(() => {
     axios
       .get(`https://iz-nc-news.onrender.com/api/articles/${article_id}`)
       .then((response) => {
-        return response.data
+        return response.data;
       })
       .then((data) => {
-        setArticle(data.article)
+        setArticle(data.article);
       });
   }, []);
 
-  return <h1 className="article">Article</h1>;
+  return (
+    <article className="article">
+      <h1>{article.title}</h1>
+      <img src={article.article_img_url} alt={article.title} />
+    <h2>{article.author}</h2>
+    <h3>{article.topic}</h3>
+      <p>{article.body}</p>
+      <p>Comment Count: {article.comment_count}</p>
+      <p>Votes: {article.votes}</p>
+      <p>{article.created_at}</p>
+    </article>
+  );
 };
 
 export default Article;
