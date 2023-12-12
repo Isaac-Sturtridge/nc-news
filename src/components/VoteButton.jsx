34f5e03e11c8beca15bcfa06131d27addf19d3d8
Isaac-Sturtridge <eyes330@yahoo.co.uk>
2023-12-12 11:14:36 +0000

    basic layout and visual checks

--- src/components/VoteButton.jsx
+++ src/components/VoteButton.jsx
@@ -6,16 +6,24 @@ const VoteButton = ({ votes, article_id }) => {
 
   function increaseVotes() {
     setViewVotes(viewVotes + 1);
-    patchArticle(article_id, 1);
+    patchArticle(article_id, 1)
+    .catch((err) => {
+        // a bit difficult to test for this I think but this should update visually if the server goes down
+        // the visual component update in this catch block should be replaced by a real error handler when doing task 12
+        setViewVotes(err)
+    });
   }
 
   function decreaseVotes() {
     setViewVotes(viewVotes - 1);
-    patchArticle(article_id, -1);
+    patchArticle(article_id, -1)
+    .catch((err) => {
+        setViewVotes(err)
+    });
   }
 
   return (
-    <section>
+    <section className="votingBooth">
       <button onClick={increaseVotes}>Upvote</button>
       {viewVotes}
       <button onClick={decreaseVotes}>Downvote</button>
