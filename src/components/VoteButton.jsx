a3950b5d3b6c21222e015a5fb71221ff3011b1cc
Isaac-Sturtridge <eyes330@yahoo.co.uk>
2023-12-12 11:42:45 +0000

    add limits on upvote/downvote buttons so each user can change the vote number by 1

--- src/components/VoteButton.jsx
+++ src/components/VoteButton.jsx
@@ -3,30 +3,56 @@ import { patchArticle } from "../utils/axios";
 
 const VoteButton = ({ votes, article_id }) => {
   const [viewVotes, setViewVotes] = useState(votes);
+  const [hasUpvoted, setHasUpvoted] = useState(false);
+  const [hasDownvoted, setHasDownvoted] = useState(false);
 
   function increaseVotes() {
-    setViewVotes(viewVotes + 1);
-    patchArticle(article_id, 1)
-    .catch((err) => {
-        // a bit difficult to test for this I think but this should update visually if the server goes down
-        // the visual component update in this catch block should be replaced by a real error handler when doing task 12
-        setViewVotes(err)
-    });
+    let inc_votes = hasDownvoted ? 2 : 1
+    if(!hasUpvoted) {
+        setHasUpvoted(true)
+        setHasDownvoted(false)
+        setViewVotes(viewVotes + inc_votes);
+        patchArticle(article_id, inc_votes)
+        .catch((err) => {
+            // a bit difficult to test for this I think but this should update visually if the server goes down
+            // the visual component update in this catch block should be replaced by a real error handler when doing task 12
+            setViewVotes(err)
+        });
+    } else {
+        setHasUpvoted(false)
+        setViewVotes(viewVotes - inc_votes)
+        patchArticle(article_id, -inc_votes)
+        .catch((err) => {
+            setViewVotes(err)
+        });
+    }
   }
 
   function decreaseVotes() {
-    setViewVotes(viewVotes - 1);
-    patchArticle(article_id, -1)
-    .catch((err) => {
-        setViewVotes(err)
-    });
+    let inc_votes = hasUpvoted ? 2 : 1
+    if(!hasDownvoted) {
+        setHasDownvoted(true)
+        setHasUpvoted(false)
+        setViewVotes(viewVotes - inc_votes);
+        patchArticle(article_id, -inc_votes)
+        .catch((err) => {
+            setViewVotes(err)
+        });
+    } else {
+        setHasDownvoted(false)
+        setViewVotes(viewVotes + inc_votes)
+        patchArticle(article_id, inc_votes)
+        .catch((err) => {
+            setViewVotes(err)
+        });
+    }
   }
 
   return (
     <section className="votingBooth">
-      <button onClick={increaseVotes}>Upvote</button>
+      <button onClick={increaseVotes}>{hasUpvoted ? "Undo upvote" : "Upvote"}</button>
       {viewVotes}
-      <button onClick={decreaseVotes}>Downvote</button>
+      <button onClick={decreaseVotes}>{hasDownvoted ? "Undo downvote" : "Downvote"}</button>
     </section>
   );
 };
