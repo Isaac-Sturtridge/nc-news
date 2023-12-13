ec4e453606ec0f52f50a703afebb8a2edd9bca4e
Isaac-Sturtridge <eyes330@yahoo.co.uk>
2023-12-13 14:12:48 +0000

    adds topic filter to article list, reached by getting it off the URL with useLocation

--- src/components/TopicCard.jsx
+++ src/components/TopicCard.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 const TopicCard = ({topic}) => {
+
     return (
     <article className="topic">
+    <Link to={`/articles?topic=${topic.slug}`}>
     <h1>{topic.slug}</h1>
+    </Link>
     <p>{topic.description}</p>
     </article>
     )
